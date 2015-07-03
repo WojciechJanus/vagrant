2e03f0df0518dbd552d9b0279c6fb02a960b4fba
Wojciech Janus <wojciech.janus@jjsoft.info>
2015-07-03 18:50:37 +0100

    Added unzip

--- java8/java8.sh
+++ java8/java8.sh
@@ -6,13 +6,13 @@ sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update -y
 
-sudo apt-get -qq install build-essential rsync telnet screen man wget -y
-sudo apt-get -qq install strace tcpdump -y
-sudo apt-get -qq install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
-sudo apt-get -qq install software-properties-common python-software-properties -y
+sudo apt-get install build-essential rsync telnet screen man wget -y
+sudo apt-get install strace tcpdump -y
+sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
+sudo apt-get install software-properties-common python-software-properties -y
+sudo apt-get install unzip
 
-sudo apt-get -qq install oracle-java8-installer -y
+sudo apt-get install oracle-java8-installer -y
 
 sudo echo "Installing Git"
 sudo apt-get install git -y
-
